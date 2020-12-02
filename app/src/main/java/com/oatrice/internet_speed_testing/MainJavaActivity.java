2176ac489bea635cc77b3c58f4938a76f4dd9dc7
Praveenb <p.boyalapalli@pccsuk.com>
2020-12-02 11:47:43 +0530

    v1.1 Handled Complete listener handle

--- app/src/main/java/com/oatrice/internet_speed_testing/MainJavaActivity.java
+++ app/src/main/java/com/oatrice/internet_speed_testing/MainJavaActivity.java
@@ -58,7 +58,7 @@ public class MainJavaActivity extends AppCompatActivity {
 
             }
         });
-        builder.start("https://www.skylinesynergy.net/AppTest/app-speed-test.zip","https://pccsuk.com/empty.php", 1);
+        builder.start("","", 1);//please use Skyline server urls
 
         //checkSpeed();
 
